7d7b4c6906ff9162ff5541f598c0608836652879
Артём Лебедев <artem-lebedev-2004@inbox.ru>
2026-02-16 23:53:43 +0400

    gghj

--- src/components/sections/ServicesSection/blurPhoto.vue
+++ src/components/sections/ServicesSection/blurPhoto.vue
@@ -1,15 +1,21 @@
 <template>
     <transition>
-        <div v-if="isClickOnDolls" class="absolute -inset-x-[0.3%] -inset-y-[0.5%]">
-            <svg width="100%" height="100%" viewBox="0 0 311 174" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <foreignObject x="-11" y="-11" width="333" height="196"><div xmlns="http://www.w3.org/1999/xhtml" style="backdrop-filter:blur(5.5px);clip-path:url(#bgblur_0_1361_57_clip_path);height:100%;width:100%"></div></foreignObject><path data-figma-bg-blur-radius="11" d="M261 0C261 19.8822 277.118 36 297 36C301.966 36 306.696 34.9941 311 33.1758V142C311 159.673 296.673 174 279 174H32C14.3269 174 0 159.673 0 142V32C0 14.3269 14.3269 0 32 0H261Z" fill="#FFD4DF" fill-opacity="0.69"/>
-                <defs>
-                <clipPath id="bgblur_0_1361_57_clip_path" transform="translate(11 11)"><path d="M261 0C261 19.8822 277.118 36 297 36C301.966 36 306.696 34.9941 311 33.1758V142C311 159.673 296.673 174 279 174H32C14.3269 174 0 159.673 0 142V32C0 14.3269 14.3269 0 32 0H261Z"/>
-                </clipPath></defs>
+        <div v-if="isClickOnDolls" class="absolute -inset-[0.5%] ">
+            <svg @click="$emit('isClickOnBlur')" width="100%" height="100%" viewBox="0 0 311 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <foreignObject x="-11" y="-11" width="333" height="194"><div xmlns="http://www.w3.org/1999/xhtml" style="backdrop-filter:blur(5.5px);clip-path:url(#bgblur_0_1361_57_clip_path);height:100%;width:100%"></div></foreignObject><path data-figma-bg-blur-radius="11" d="M261.015 0C261.544 19.4198 277.452 35 297 35C301.966 35 306.696 33.9941 311 32.1758V140C311 157.673 296.673 172 279 172H32C14.3269 172 0 157.673 0 140V32C2.70606e-06 14.3269 14.3269 0 32 0H261.015Z" fill="#FFD4DF" fill-opacity="0.69"/>
+              <defs>
+              <clipPath id="bgblur_0_1361_57_clip_path" transform="translate(11 11)"><path d="M261.015 0C261.544 19.4198 277.452 35 297 35C301.966 35 306.696 33.9941 311 32.1758V140C311 157.673 296.673 172 279 172H32C14.3269 172 0 157.673 0 140V32C2.70606e-06 14.3269 14.3269 0 32 0H261.015Z"/>
+              </clipPath></defs>
             </svg>
+
+            <div class="absolute w-max h-max m-auto inset-0 text-center">
+              <div>УСЛУГИ</div>
+              <div>ПЕРСОНАЖИ</div>
+              <div>ЗАКАЗАТЬ</div>
+            </div>
         </div>
     </transition>
-    
+
 </template>
 
 <script setup>
@@ -17,6 +23,7 @@ import { toRefs } from 'vue';
 
 const props = defineProps(['isClickOnDolls'])
 const {isClickOnDolls} = toRefs(props)
+
 </script>
 
 <style scoped>
